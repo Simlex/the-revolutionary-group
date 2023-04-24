f69871b22960da5d140c5f2abd2ad04d12a69f8f
Simlex <gettosimlex@gmail.com>
2023-04-24 19:18:21 +0100

    Updated conditon for app ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,41 @@ import '@/styles/globals.scss'
 import type { AppProps } from 'next/app'
 import Layout from '../../components/Layout'
 import 'aos/dist/aos.css';
+import { useEffect, useState } from 'react';
+import styles from '../styles/Welcome.module.scss';
+import Image from 'next/image';
+import images from '../../public/images';
 
 export default function App({ Component, pageProps }: AppProps) {
+
+  const [isWelcomeScreenVisible, setIsWelcomeScreenVisible] = useState(true);
+  const [isWelcomeScreenStyle, setIsWelcomeScreenStyle] = useState(true);
+
+  useEffect(() => {
+    if (!isWelcomeScreenStyle) {
+      setTimeout(() => {
+        setIsWelcomeScreenVisible(false);
+      }, 500);
+    }
+  }, [isWelcomeScreenStyle])
+
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  ) 
+    <>
+      {
+        isWelcomeScreenVisible ?
+          <div className={isWelcomeScreenStyle ? styles.welcomeScreen : styles.welcomeScreenClose}>
+            <div className={styles.image}>
+              <Image src={images.andrew} alt='Andrew' />
+            </div>
+            <span>ANDREW GODWIN</span>
+            <p>Welcomes you to <span>revelatorygeneration.net</span></p>
+            <button onClick={() => setIsWelcomeScreenStyle(false)}>Continue to site</button>
+          </div>
+          :
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+      }
+    </>
+  )
 }
