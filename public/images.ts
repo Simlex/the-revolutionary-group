b5514dc4d5c56f0c62e3dd4b197bd7e9739c8006
Simlex <gettosimlex@gmail.com>
2023-04-24 19:17:17 +0100

    Updated assets

--- public/images.ts
+++ public/images.ts
@@ -2,6 +2,7 @@ import logo from "./images/logo.png";
 import andrew from "./images/andrew.jpg";
 import He_Has_Done_It_All_Cover from "./images/He_Has_Done_It_All_Cover.jpg";
 import scroll_down from "./images/scroll_down.svg";
+import musicBg from "./images/music_bg.svg";
 // import spotify from "./images/spotify.jpg";
 // import youtube from "./images/youtube.jpg";
 // import instagram from "./images/instagram.jpg";
@@ -11,5 +12,6 @@ export default {
   logo,
   andrew,
   scroll_down,
-  He_Has_Done_It_All_Cover
+  He_Has_Done_It_All_Cover,
+  musicBg
 };
